dc9e82a2c9806a65d1368a940f664185027f7b8d
Ashish <ashishvpatel797@gmail.com>
2019-08-06 16:13:32 -0500

    removed extra spaces and comments

--- Firebase To Do List/newViewController.swift
+++ Firebase To Do List/newViewController.swift
@@ -9,31 +9,14 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
-//var listArray = [String] ()
 class newViewController: UIViewController {
 var ref: DatabaseReference!
     @IBOutlet weak var myTextField: UITextField!
     @IBAction func submitButtonTapped(_ sender: Any) {
-       
         ref = Database.database().reference().child("Student")
         ref.childByAutoId().child("name").setValue(myTextField.text)
-       
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
